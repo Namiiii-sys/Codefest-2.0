c591020b02d97a16d4b54686855ff96d478bfaac
RenItsuki <renitsuki09@gmail.com>
2026-02-21 17:23:57 +0530

    registration links

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: {
-    default: "CodeFest 2.0 | National Hackathon & Tech Fest",
+    default: "CodeFest 2.0 | Flagship Tech Fest of SSCBS",
     template: "%s | CodeFest 2.0",
   },
 
   description:
-    "CodeFest 2.0 is a national-level hackathon and technical fest featuring coding competitions, design challenges, workshops, and innovation-driven problem solving.",
+    "CodeFest 2.0 is a flagship technical fest featuring coding competitions, design challenges, workshops, and innovation-driven problem solving.",
 
   keywords: [
     "CodeFest",
@@ -29,26 +29,18 @@ export const metadata: Metadata = {
     "Code Festival",
   ],
 
-  authors: [
-    {
-      name: "Namita Mehra",
-    },
-  ],
 
-  creator: "Namita Mehra",
-  publisher: "CodeFest 2.0",
-
-  metadataBase: new URL("https://codefest-9eq.pages.dev"), // change if domain differs
+  metadataBase: new URL("https://codefest26.acmsscbs.in"), 
 
   openGraph: {
-    title: "CodeFest 2.0 | National Hackathon & Tech Fest",
+    title: "CodeFest 2.0 | Flagship Tech Fest of SSCBS",
     description:
-      "Join CodeFest 2.0 – a national-level hackathon with exciting coding events, innovation challenges, and real-world problem solving.",
-    url: "https://codefest-9eq.pages.dev",
+      "Join CodeFest 2.0 – a flagship tech fest with exciting coding events, innovation challenges, and real-world problem solving.",
+    url: "https://codefest26.acmsscbs.in",
     siteName: "CodeFest 2.0",
     images: [
       {
-        url: "/LOGO.png", // add later
+        url: "/LOGO.png", 
         width: 1200,
         height: 630,
         alt: "CodeFest 2.0",
@@ -60,7 +52,7 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: "CodeFest 2.0 | National Hackathon & Tech Fest",
+    title: "CodeFest 2.0 | Flagship Tech Fest of SSCBS",
     description:
       "National-level hackathon & tech fest with coding competitions, design challenges, and innovation.",
     images: ["/LOGO.png"],
